03850f38d8f770d9911a88da49c1797834f2aa48
Mich Seixas <mcseixas@gmail.com>
2023-04-23 01:42:42 +0200

    rectangles green and grey

--- js/index.js
+++ js/index.js
@@ -5,3 +5,24 @@ window.onload = () => {
 
   function startGame() {}
 };
+
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green"; // small green left
+ctx.fillRect(0, 0, 30, 700);
+
+ctx.fillStyle = "green"; // small green right
+ctx.fillRect(470, 0, 30, 700);
+
+ctx.fillStyle = "grey"; // small grey left
+ctx.fillRect(30, 0, 10, 700);
+
+ctx.fillStyle = "grey"; // small grey right
+ctx.fillRect(460, 0, 10, 700);
+
+ctx.fillStyle = "grey"; // big grey center
+ctx.fillRect(50, 0, 400, 700);
+
+
